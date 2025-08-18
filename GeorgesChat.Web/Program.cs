e5337fc8e32dc64e6a6c536101894579d310c811
Georgi Popov <georgi.popov@rentallsoftware.com>
2025-08-18 10:53:21 +0300

    Fix sing in maganer and some  style fixes

--- GeorgesChat.Web/Program.cs
+++ GeorgesChat.Web/Program.cs
@@ -10,9 +10,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("GeorgesChatDbContextConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<GeorgesChatDbContext>(options =>
-    options.UseSqlServer(connectionString));
+	options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
@@ -24,13 +24,15 @@ builder.Services.AddDefaultIdentity<User>(options =>
 	options.Password.RequireUppercase = false;
 	options.SignIn.RequireConfirmedAccount = false;
 })
+	.AddSignInManager<User>()
    .AddEntityFrameworkStores<GeorgesChatDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddSignalR();
 
 builder.Services.AddTransient<IChatService, ChatService>();
-builder.Services.AddTransient<IUserService,UserService>();
+builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IMessageService, MessageService>();
 
 var app = builder.Build();
@@ -38,13 +40,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+	app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -55,8 +57,8 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 app.MapHub<ChatHub>("/chatHub");
 
